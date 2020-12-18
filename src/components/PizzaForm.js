5926689e65e8ef501ad145e04d0dac4a9f12e702
Chance Swartz <chanceswartz@gmail.com>
2020-12-18 12:12:10 -0600

    Built out PizzaForm

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -7,13 +7,103 @@
 // Text Input for Special Instructions
 // Add to Order button that submits form, returning database of name, size, toppings, instructions
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function PizzaForm(props) {
-    const { value, submit, change, disabled, errors } = props
+  const { values, submit, change, disabled, errors } = props;
 
-    return(
-        null
-    )
+  const onSubmit = (event) => {
+    event.preventDefault();
+    submit();
+  };
+
+  const onChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    const useValue = type === "checkbox" ? checked : value;
+    change(name, useValue);
+  };
+
+  return (
+    <form className="PizzaForm-container" onSubmit={onSubmit}>
+      <h2> Build a Pizza! </h2>
+      <div className="PizzaForm group inputs">
+        <label>
+          Name
+          <input
+            value={values.name}
+            onChange={onChange}
+            name="name"
+            type="text"
+          />
+        </label>
+        <br />
+        <label>
+          Size
+          <select onChange={onChange} value={values.size} name="size">
+            <option value=""> Select an Option </option>
+            <option value="small"> Small </option>
+            <option value="medium"> Medium </option>
+            <option value="large"> Large </option>
+          </select>
+        </label>
+        <br />
+        <label>
+          Pineapple
+          <input
+            type="checkbox"
+            name="pineapple"
+            checked={values.pineapple}
+            onChange={onChange}
+          />
+        </label>
+        <br />
+        <label>
+          Jalepeno
+          <input
+            type="checkbox"
+            name="jalepeno"
+            checked={values.jalepeno}
+            onChange={onChange}
+          />
+        </label>
+        <br />
+        <label>
+          Mandarin Oranges
+          <input
+            type="checkbox"
+            name="mandOranges"
+            checked={values.mandOranges}
+            onChange={onChange}
+          />
+        </label>
+        <br />
+        <label>
+          Ham
+          <input
+            type="checkbox"
+            name="ham"
+            checked={values.ham}
+            onChange={onChange}
+          />
+        </label>
+        <br />
+        <label>
+          Special Instructions
+          <input
+            value={values.specIns}
+            onChange={onChange}
+            name="specIns"
+            type="text"
+          />
+        </label>
+      </div>
+      <div className="PizzaForm-group-submit">
+        <button disabled={disabled}> Add to Order </button>
+        <div className="Errors">
+          <div>{errors.name}</div>
+          <div>{errors.size}</div>
+        </div>
+      </div>
+    </form>
+  );
 }
-
