e1f756961e28bd06473ea6bf0afa133f30295c70
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-01-29 11:33:54 -0600

    Should've installed just about everything I need, have created files for the seperate pages, including Schema and fakeData. Handled imports on App, some of the state.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import Home from "./Components/Home";
+import PizzaForm from "./Components/PizzaForm";
+import schema from "./Validation/PizzaSchema";
+import Pizza from "./Components/Pizza";
+import * as yup from "yup";
+import data from "./Validation/fakeData"
+// Initial States
+const initialFormValues = {
+  // text inputs
+  name: "",
+  specIns: "", //Special Instructions
+  // dropdown
+  size: "",
+  // checkboxes
+  pineapple: false,
+  jalepeno: false,
+  mandOranges: false,
+  ham: false,
+};
+
+const initialFormErrors = {
+  name: "",
+};
+
+const initialPizzas = [];
+
+function fetchPizzas() {
+  return Promise.resolve({ success: true, data })
+}
 
 const App = () => {
   return (
